e212963d0d1df82a32d3e5d93ce7a684613b0612
Otero Ugalde Karla Jimena <22031343@itcelaya.edu.mx>
2025-10-15 22:05:31 -0600

    DAOV1 con Singleton 15/10/25

--- DAOV1/src/Estudiante.java
+++ DAOV1/src/Estudiante.java
@@ -1,14 +1,32 @@
 public class Estudiante {
+    private static volatile Estudiante instancia;
+
     private int id;
     private String nombre;
     private String correo;
 
-    // Constructores
-    public Estudiante() {}
-    public Estudiante(int id, String nombre, String correo) {
+    //Se le implemento patrón Singleton double-checked locking para hacer la inicialización más temprana, este metodo es bueno
+    //cuando se tiene el multi-hilo (no es el caso pero es el más conveniente usar debido al DAO)
+
+    // Constructor privado
+    private Estudiante() {}
+
+    Estudiante(int id, String nombre, String correo) {
         this.id = id;
         this.nombre = nombre;
         this.correo = correo;
+    }
+
+    // Metodo con double-checked locking para mejor performance
+    public static Estudiante getInstancia() {
+        if (instancia == null) {
+            synchronized (Estudiante.class) {
+                if (instancia == null) {
+                    instancia = new Estudiante();
+                }
+            }
+        }
+        return instancia;
     }
 
     // Getters y Setters
